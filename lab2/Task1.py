8464011ad4c1b3c11a41452bb874c07bbb65e790
Jiaxin Yang <yyjx0503@gmail.com>
2022-09-29 13:07:01 -0400

    lab2 set
    python3 -m venv .venv
    source .venv/bin/activate

--- lab2/Task1.py
+++ lab2/Task1.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 df = pd.read_csv('organised_Gen.csv')
-
-print(df.to_string()) 
+OH_data = df.loc[(df['STATE'] == 'OH') & (df['YEAR'] >= 2017) & (df['YEAR'] <= 2021)]
+print(OH_data)
