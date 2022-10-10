d69019232b8aec2e35b1ee2b24e74628af39d30d
Jiaxin-yyjx <yyjx0503@gmail.com>
2022-10-09 23:15:59 -0400

    Update Task1.py

--- lab2/Task1.py
+++ lab2/Task1.py
@@ -3,3 +3,5 @@ import pandas as pd
 df = pd.read_csv('organised_Gen.csv')
 OH_data = df.loc[(df['STATE'] == 'OH') & (df['YEAR'] >= 2017) & (df['YEAR'] <= 2021)]
 print(OH_data)
+
+sum_Generation = df.groupby(['YEAR','ENERGY SOURCE'])
