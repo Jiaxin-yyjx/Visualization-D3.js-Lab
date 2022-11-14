5309a5828ba62115abe223e792993421606ee2a8
Jiaxin Yang <yyjx0503@gmail.com>
2022-11-14 18:56:59 -0500

    1

--- lab4/Task1.py
+++ lab4/Task1.py
@@ -5,10 +5,12 @@ import pandas as pd
 df = pd.read_csv('electricity_generation.csv')
 
 # 2. Find total generation (TYPE OF PRODUCER is 'Total Electric Power Industry' and ENERGY SOURCE is 'Total') data records from 2021/01 to 2021/12 (US-TOTAL for STATE is excluded)
-df_2021 = df.loc[(df['YEAR'] == 2021)]
+df_2021 = df.loc[(df['YEAR'] == 2021) & (df['STATE'] != 'US-TOTAL') & (df['ENERGY SOURCE'] == 'Total') & (df['TYPE OF PRODUCER'] == 'Total Electric Power Industry')]
 print(df_2021)
 
 # 3. Output a CSV file which should contain 3 columns: MONTH, STATE, GENERATION(Mwh)
+mon_stat_gen = df_2021.loc[:,['MONTH', 'STATE','GENERATION (Megawatthours)']]
+mon_stat_gen.to_csv('mon_stat_gen.csv', encoding='utf-8', index=False)
 
 # 4. According to the original energy data, select 2021 monthly data based on TYPE OF PRODUCER. (Note that ENERGY SOURCE is 'Total')
 
