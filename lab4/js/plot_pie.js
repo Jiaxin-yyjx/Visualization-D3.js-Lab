f220796957313a40912ab98bb61e3cddb937eacc
Jiaxin-yyjx <yyjx0503@gmail.com>
2022-11-18 16:54:29 -0500

    00

--- lab4/js/plot_pie.js
+++ lab4/js/plot_pie.js
@@ -1,20 +1,20 @@
 // TASK 4 - D3 Pie Chart
 // set the dimensions and margins of the graph
-let width = 450
-    height = 450
-    margin = 40
+let width = 450,
+  height = 450,
+  margin = 40;
 
 // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
-let radius = Math.min(width, height) / 2 - margin
+let radius = Math.min(width, height) / 2 - margin;
 
 // append the svg object to the div called 'pie'
-let svg = d3.select("#pie")
+let svg = d3
+  .select("#pie")
   .append("svg")
-    .attr("width", width)
-    .attr("height", height)
+  .attr("width", width)
+  .attr("height", height)
   .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
+  .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
 d3.csv("type_Gen.csv", pieChart);
 
@@ -32,7 +32,8 @@ function pieChart(error, typePro) {
   var color = d3.scaleOrdinal(d3["schemeCategory10"]);
   var arcGenerator = d3.arc().innerRadius(30).outerRadius(100);
 
-  svg.append("g")
+  svg
+    .append("g")
     .selectAll("path")
     .data(arcData)
     .enter()
@@ -42,13 +43,15 @@ function pieChart(error, typePro) {
       return color(d.data["TYPE OF PRODUCER"]);
     });
 
-    svg.append("g")
+  svg
+    .append("g")
     .append("circle")
     .attr("cx", -150)
     .attr("cy", 150)
     .attr("r", 6)
     .style("fill", "#1f77b4");
-    svg.append("g")
+  svg
+    .append("g")
     .append("text")
     .attr("x", -130)
     .attr("y", 150)
@@ -56,13 +59,15 @@ function pieChart(error, typePro) {
     .style("font-size", "16px")
     .attr("alignment-baseline", "middle");
 
-    svg.append("g")
+  svg
+    .append("g")
     .append("circle")
     .attr("cx", -150)
     .attr("cy", 175)
     .attr("r", 6)
     .style("fill", "#ff7f0e");
-    svg.append("g")
+  svg
+    .append("g")
     .append("text")
     .attr("x", -130)
     .attr("y", 175)
@@ -70,13 +75,15 @@ function pieChart(error, typePro) {
     .style("font-size", "16px")
     .attr("alignment-baseline", "middle");
 
-    svg.append("g")
+  svg
+    .append("g")
     .append("circle")
     .attr("cx", -150)
     .attr("cy", 200)
     .attr("r", 6)
     .style("fill", "#2ca02c");
-    svg.append("g")
+  svg
+    .append("g")
     .append("text")
     .attr("x", -130)
     .attr("y", 200)
@@ -84,13 +91,15 @@ function pieChart(error, typePro) {
     .style("font-size", "16px")
     .attr("alignment-baseline", "middle");
 
-    svg.append("g")
+  svg
+    .append("g")
     .append("circle")
     .attr("cx", -150)
     .attr("cy", 225)
     .attr("r", 6)
     .style("fill", "#d62728");
-    svg.append("g")
+  svg
+    .append("g")
     .append("text")
     .attr("x", -130)
     .attr("y", 225)
@@ -98,13 +107,15 @@ function pieChart(error, typePro) {
     .style("font-size", "16px")
     .attr("alignment-baseline", "middle");
 
-    svg.append("g")
+  svg
+    .append("g")
     .append("circle")
     .attr("cx", -150)
     .attr("cy", 250)
     .attr("r", 6)
     .style("fill", "#9467bd");
-    svg.append("g")
+  svg
+    .append("g")
     .append("text")
     .attr("x", -130)
     .attr("y", 250)
